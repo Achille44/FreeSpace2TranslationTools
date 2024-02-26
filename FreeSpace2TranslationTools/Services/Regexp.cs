ee4030f24048ade5e0add34457cb224a0a109cae
Achille44 <achille.parmentier@pm.me>
2024-02-26 18:24:55 +0100

    bugfix: subsystem regex too sensitive

--- FreeSpace2TranslationTools/Services/Regexp.cs
+++ FreeSpace2TranslationTools/Services/Regexp.cs
@@ -313,7 +313,7 @@ namespace FreeSpace2TranslationTools.Services
         private static partial Regex _ShipNames();
         public static Regex ShipNames { get => _ShipNames(); }
 
-		[GeneratedRegex("(?<!;)\\$Subsystem:.*?(?=\\$Subsystem:|\\r?\\n\\r?\\n)", RegexOptions.Singleline | RegexOptions.IgnoreCase)]
+		[GeneratedRegex("(?<!;)\\$Subsystem:.*?(?=\\$Subsystem:|\\r?\\n[ \t]*\\r?\\n)", RegexOptions.Singleline | RegexOptions.IgnoreCase)]
 		private static partial Regex _Subsystems();
 		public static Regex Subsystems { get => _Subsystems(); }
 
